cac6b8a5898871d4f6c52bbbb269e0463c754aba
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-05-22 10:17:58 +0700

    javascript|Pengkondisian| If bersarang

--- JS/Pengkondisian/app.js
+++ JS/Pengkondisian/app.js
@@ -1,13 +1,27 @@
-let nilai = prompt("Masukan Nilai : ");
+// let nilai = prompt("Masukan Nilai : ");
 
-if (nilai > 90) {
-  alert("A");
-} else if (nilai > 80) {
-  alert("B");
-} else if (nilai > 70) {
-  alert("C");
-} else if (nilai > 60) {
-  alert("D");
+// if (nilai > 90) {
+//   alert("A");
+// } else if (nilai > 80) {
+//   alert("B");
+// } else if (nilai > 70) {
+//   alert("C");
+// } else if (nilai > 60) {
+//   alert("D");
+// } else {
+//   alert("E");
+// }
+
+const pass = prompt("masukan password");
+// cek apakah password lebih dari 6 karakter
+
+if (pass.length >= 6) {
+  // cek apakah password ada spasi
+  if (pass.indexOf(" ") === -1) {
+    console.log("password valid");
+  } else {
+    console.log("password tidak boleh ada spasi");
+  }
 } else {
-  alert("E");
+  console.log("password minimal 6 karakter");
 }
