a2fb78c222f8f5fa1ffa6298d77fff5e631a8564
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-05-22 09:52:39 +0700

    Javascript|Pengkondisian| Else If statemant

--- JS/IF statement/app.js
+++
@@ -1,6 +0,0 @@
-let angka = Math.random();
-console.log(angka);
-
-if (angka >= 0.5) {
-  console.log("Angka lebih besar dari 0.5");
-}
---
+++ JS/Pengkondisian/app.js
@@ -0,0 +1,13 @@
+let nilai = prompt("Masukan Nilai : ");
+
+if (nilai > 90) {
+  alert("A");
+} else if (nilai > 80) {
+  alert("B");
+} else if (nilai > 70) {
+  alert("C");
+} else if (nilai > 60) {
+  alert("D");
+} else {
+  alert("E");
+}
