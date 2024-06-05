530467620274121bc5d89b41e04c8b0ae34f78f9
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-06-05 10:37:32 +0700

    Javascript | Perulangan | While dan penggunaan break pada perulangan

---
+++ JS/Perulangan/app.js
@@ -0,0 +1,13 @@
+// while loop
+
+// let pass = "Pas123";
+// guest = prompt("masukan password!");
+// while (guest !== pass) {
+//   guest = prompt("masukan password!");
+// }
+// alert("password benar");
+
+for (let i = 0; i < 1000; i++) {
+  console.log(i);
+  if (i === 100) break;
+}
