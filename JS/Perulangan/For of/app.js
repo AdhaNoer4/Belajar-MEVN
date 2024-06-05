67358880794789bdef59051c0f727f814ea5b139
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-06-05 11:38:26 +0700

    Javascript | Perulangan | For of

---
+++ JS/Perulangan/For of/app.js
@@ -0,0 +1,17 @@
+let fruits = ["banana", "Apple", "Melon", "Avocado"];
+
+for (const buah of fruits) {
+  console.log(buah);
+}
+
+let nama = [
+  ["Adha", "Syarif", "Arfia"],
+  ["Andi", "Fitri", "Budi"],
+  ["Bambang", "Harith", "Masha"],
+];
+
+for (const name of nama) {
+  for (const jeneng of name) {
+    console.log(jeneng);
+  }
+}
