6cfd1d7d801b565a51b63203b3f483e43ed0b25d
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-06-06 08:03:27 +0700

    Javascript | Function | Parameter & Argument

---
+++ JS/Function/parameter & argument/app.js
@@ -0,0 +1,6 @@
+function selamatPagi(name) {
+  // di dalam kurung ini dinamakan Parameter
+  console.log(`Selamat Pagi, ${name} dan Selamat datang!`);
+}
+
+selamatPagi("Adha"); // <-- di dalam kurung ini dinamakan Argument
