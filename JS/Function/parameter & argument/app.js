52bf31e1622fa3ad43f3061874622b51597bd620
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-06-06 08:19:03 +0700

    Javascript | Function | Multiple Parameter & Argument

--- JS/Function/parameter & argument/app.js
+++ JS/Function/parameter & argument/app.js
@@ -1,6 +1,23 @@
+// parameter pada fungsi digunakan untuk input
 function selamatPagi(name) {
   // di dalam kurung ini dinamakan Parameter
   console.log(`Selamat Pagi, ${name} dan Selamat datang!`);
 }
 
 selamatPagi("Adha"); // <-- di dalam kurung ini dinamakan Argument
+
+// Multiple Parameter & Argument
+
+function jumlah(a, b) {
+  const hitung = a + b;
+  console.log(hitung);
+}
+
+// jika parameter mau digunakan tanpa input terrlebih dahulu,
+// tambahkan nilai default pada parameter
+//contoh
+
+function jumlah(a = 0, b = 0) {
+  const hitung = a + b;
+  console.log(hitung);
+}
