5a4d747e6f80e143a0659816bbe4350ebda22f43
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-06-08 09:55:52 +0700

    Javascript | Function | Function Expression

---
+++ JS/Function/Function Expression/app.js
@@ -0,0 +1,15 @@
+// function Normal
+
+function perpangkatan(nilai) {
+  return nilai * nilai;
+}
+
+let hasil = perpangkatan(10);
+
+// Function Expression
+
+let hasilPerpangkatan = function (nilai) {
+  return nilai * nilai;
+};
+
+// penggunakan function Expression dapat meringkas penulisan function
