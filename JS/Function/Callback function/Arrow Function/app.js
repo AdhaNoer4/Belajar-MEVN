14b4dfa71aa35983855498c80de08fc34d470678
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-06-12 09:10:05 +0700

    Javascript | Function | Arrow Function

---
+++ JS/Function/Callback function/Arrow Function/app.js
@@ -0,0 +1,28 @@
+// Arrow Function
+//  adalah function expression yang di persingkat.
+
+// Contoh perbandiagan
+
+// Function biasa
+
+// function perpangkatan(x) {
+//   return x * x;
+// }
+
+// const hasil = perpangkatan(5);
+
+// Function Expression
+
+// const hasil = function (x) {
+//   return x * x;
+// };
+
+// Arrow Function
+
+const hasil = (x) => {
+  return x * x;
+};
+
+const rand = () => {
+  return Math.floor(Math.random() * 100) + 1;
+};
