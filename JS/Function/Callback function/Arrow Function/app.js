3f59ef1f6d90e6ce9a6ceaf5f3fa0a937c16c690
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-06-12 09:23:17 +0700

    Javascript | Function | Implisit Arrow Function

--- JS/Function/Callback function/Arrow Function/app.js
+++ JS/Function/Callback function/Arrow Function/app.js
@@ -19,10 +19,19 @@
 
 // Arrow Function
 
-const hasil = (x) => {
-  return x * x;
-};
+// const hasil = (x) => {
+//   return x * x;
+// };
 
 const rand = () => {
   return Math.floor(Math.random() * 100) + 1;
 };
+
+// Implisit Arrow Function
+// adalah Arrow function yang dapat tanpa menggunakan return untuk menyimpan nilai ke dalam variable
+// Implisit arrow function hanya bisa digunakan pada satu baris proses / logila saja.
+// Implisit arrow function menggunakan () bukan {} untuk menulis dengan beda baris
+
+// CONTOH
+
+const random = () => Math.floor(Math.random() * 100) + 1;
