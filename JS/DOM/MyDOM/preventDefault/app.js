3c40b280de63d6663eedc07aa77e67b7bb66a34b
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-08-01 09:35:59 +0700

    Javascript | DOM | Membuat Aplikasi Notes

--- JS/DOM/MyDOM/preventDefault/app.js
+++ JS/DOM/MyDOM/preventDefault/app.js
@@ -1,11 +1,17 @@
 const form = document.querySelector("form");
+const input = document.querySelector("input");
+const list = document.querySelector("#note");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault(); // preventDefault berfungsi agar form setelah di submit tidak pindah halaman
-  console.log("submitted");
+  const noteValue = input.value;
+  const newList = document.createElement("li");
+  newList.innerText = noteValue;
+  list.append(newList);
+  input.value = "";
 });
 
-document.querySelector("a").addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log('link di klik');
-});
+// document.querySelector("a").addEventListener("click", (e) => {
+//   e.preventDefault();
+//   console.log('link di klik');
+// });
