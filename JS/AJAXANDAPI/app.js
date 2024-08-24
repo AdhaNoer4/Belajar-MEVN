cfd52e55cb63a53ee1acfbd07304ac14fc1f7a70
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-08-24 07:04:42 +0700

    Javascript | AJAX and API | Menggunakan library untuk request data from API

--- JS/AJAXANDAPI/app.js
+++ JS/AJAXANDAPI/app.js
@@ -15,17 +15,42 @@
 
 // request api dengan fetch async await
 
-const loadpeople = async () => {
+// const loadpeople = async () => {
+//   try {
+//     const res = await fetch("https://swapi.dev/api/people/1");
+//     const data = await res.json();
+//     console.log(data);
+//     const res2 = await fetch("https://swapi.dev/api/people/2");
+//     const data2 = await res2.json();
+//     console.log(data2);
+//   } catch (error) {
+//     console.log("Error", err);
+//   }
+// };
+
+// loadpeople();
+
+// Mengunakan library axios untuk request data from api
+axios
+  .get("https://swapi.dev/api/people/5")
+  .then((res) => {
+    console.log(res.data);
+  })
+  .catch((err) => {
+    console.log(err);
+    alert(err.message);
+  });
+
+// Axios async await
+
+const getPeople = async (id) => {
   try {
-    const res = await fetch("https://swapi.dev/api/people/1");
-    const data = await res.json();
-    console.log(data);
-    const res2 = await fetch("https://swapi.dev/api/people/2");
-    const data2 = await res2.json();
-    console.log(data2);
-  } catch (error) {
-    console.log("Error", err);
+    const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+    console.log(res.data);
+  } catch (err) {
+    console.log(err);
+    console.log(err.message);
+    console.log(err.response.status);
+    console.log(err.response.data);
   }
 };
-
-loadpeople();
