f8d808d20acb435f74179b98846945c5b1374470
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-08-28 09:59:32 +0700

    Javascript| OOP | Factory Function : Color convert

--- JS/AJAXANDAPI/tvshow/main.js
+++ JS/AJAXANDAPI/tvshow/main.js
@@ -2,16 +2,18 @@ const form = document.querySelector("#search-form");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  document.querySelectorAll('img').forEach((img) => img.remove());
-  const keyword = form.elements.query.value;
+  document.querySelectorAll('img').forEach((img) => img.remove()); // untuk menghapus seluruh gambar ketika menginputkan judul baru
+  const keyword = form.elements.query.value; // mengambil value dari input dengan name='query'
   const config = {
     params: { q: keyword },
   };
   const res = await axios.get("https://api.tvmaze.com/search/shows", config);
   getImg(res.data);
-  form.elements.query.value = '';
+  form.elements.query.value = ''; // untuk mengosongkan input text
 });
 
+
+// function untuk menampilkan gambar
 const getImg = (shows) =>{
     for (let result of shows){
         if (result.show.image) {
