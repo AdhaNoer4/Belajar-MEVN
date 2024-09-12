0fd250a6bb628720485cd701acd437e32cebb156
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-09-12 15:08:33 +0700

    Vue.js | Basic Vue | Reactive data pada Vue.js

--- Vue JS/install vue with CDN/app.js
+++ Vue JS/install vue with CDN/app.js
@@ -1,6 +1,11 @@
+var product = {
+    "name": "Bangboo Jacket",
+    "description":"ini adalah jaket untuk bangboo",
+    "price": "Rp 10.000"
+}
+
+// Reactive data pada Vuejs adalah kita dapat menghubungkan element dengan property dari objek js yang dapat kita ubah dengan mudah dan element juga akan ikut berubah secara real time
 var app = new Vue({
     el: '#app',
-    data: {
-      name: 'Vue.js',
-    },
+    data: product
   });
