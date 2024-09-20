0b14a1f703e0e62f97287c11d2562db8b7463576
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-09-20 09:45:56 +0700

    Vue.js | Basic vue | Binding data ke atribut dengan v-bind: / :atribut

--- Vue JS/install vue with CDN/app.js
+++ Vue JS/install vue with CDN/app.js
@@ -1,7 +1,9 @@
 var product = {
     "name": "Bangboo Jacket",
     "description":"ini adalah jaket untuk bangboo",
-    "price": "Rp 10.000"
+    "price": "Rp 10.000",
+    "image": "img/coat.jpg"
+    
 }
 
 // Reactive data pada Vuejs adalah kita dapat menghubungkan element dengan property dari objek js yang dapat kita ubah dengan mudah dan element juga akan ikut berubah secara real time
