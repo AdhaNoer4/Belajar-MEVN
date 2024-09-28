7fbc3ed5adec48ce31d37df2f72ec6b5093293d5
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-09-28 20:34:55 +0700

    Vue.js | Basic Vue | Mengenal Event dan method dalam Vue.js

--- Vue JS/install vue with CDN/app.js
+++ Vue JS/install vue with CDN/app.js
@@ -4,13 +4,22 @@ var app = new Vue({
   data: {
     maximum: 50,
     products: null,
+    cart: [],
   },
   // life cycle hooks vue.js
   mounted: function () {
     fetch("https://hplussport.com/api/products/order/price") //API
       .then((response) => response.json()) // ubah response dari api menjadi format json
-      .then((data) => { // masukan data kedalam property product
+      .then((data) => {
+        // masukan data kedalam property product
         this.products = data;
       });
+  },
+
+  // Method dalam Vue.js
+  methods: {
+    addItem: function (product) {
+      this.cart.push(product);
+    },
   },
 });
