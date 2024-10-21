de9f2451fc1387eee5f908250ce0ed6a523a7d24
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2024-10-21 08:29:57 +0700

    Vue.JS | Template Vue | perbedaan methods dan computed property di Vue.js

--- Vue JS/Computed property/app.js
+++ Vue JS/Computed property/app.js
@@ -3,12 +3,31 @@ var app = new Vue({
     data: {
         slugtext: 'The Quick #(*!&@ Brown 29'
     },
+    //omputed Property hanya akan dievaluasi ulang jika data yang terlibat berubah. 
+    //Computed Property lebih efisien karena Vue.js secara otomatis melakukan caching untuk hasil perhitungannya, sementara methods tidak memiliki caching.
     computed : {
         slugetize: function(){
             return this.slugtext
             .toLowerCase()
             .replace(/[^\w ]+/g, ' ') // Regex: menghapus tanda baca seperti #(!!*&@)
-            .replace(/ +/g, '-') // Mengubah white space / spasi menjadi tanda strip -
+            .replace(/ +/g, '-') + '-' + this.now() // Mengubah white space / spasi menjadi tanda strip -
         },
+    },
+    // Methods selalu dievaluasi setiap kali dipanggil, tanpa memedulikan apakah data yang terlibat sudah berubah atau belum
+    methods : {
+        now : function () {
+            var date = new Date();
+            return(
+
+                String(date.getHours())+
+                String(date.getMinutes())+
+                String(date.getSeconds())
+            )
+
+            
+        }
     }
+
 });
+
+//Kesimpulan: Jika kamu perlu menghitung ulang setiap kali fungsi dipanggil, gunakan methods. Jika kamu hanya ingin menghitung sesuatu ketika data yang dipakai berubah, gunakan computed property.
